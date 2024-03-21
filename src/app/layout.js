5f11a72ba4c81bf6d6fa5aab11f7f5d71fdf733b
Daniil Zaretskiy <zaretskiy03@gmail.com>
2024-03-21 23:44:36 +0300

    init project

---
+++ src/app/layout.js
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  title: "Ei nabe Restaurant",
+  description: "Ei nabe Restaurant Cases",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
